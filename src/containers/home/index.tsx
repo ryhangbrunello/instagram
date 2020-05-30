e9ae2b5eff65de06ee70a4ac6e9cca8c9fa17a86
Ryhan Brunello <ryhan.brunello@luizalabs.com>
2020-05-30 14:36:44 -0300

    Desenvolvimento do projeto Instagram

---
+++ src/containers/home/index.tsx
@@ -0,0 +1,95 @@
+import { Avatar, Button, Card, Divider, Layout, Text } from '@ui-kitten/components';
+import { Image, ScrollView, StyleSheet, View, Alert } from 'react-native';
+import React, { Component, } from 'react';
+import { inject, observer } from 'mobx-react';
+
+import { CameraApp } from '../../components/camera.component';
+import HomeStore from '../../stores/home.store';
+
+interface Props {
+  homeStore: HomeStore,
+  navigation: any;
+}
+
+@inject('homeStore')
+@observer
+export default class Home extends Component<Props> {
+
+  async componentDidMount() {
+    const { getPosts } = this.props.homeStore;
+    await getPosts();
+  }
+
+  render() {
+
+    const { posts, photoReady, toogleStatus } = this.props.homeStore;
+
+    const uploadPhoto = (uri?: string) => {
+      const { addPost } = this.props.homeStore;
+      if (uri) {
+        Alert.alert(
+          "Confirmação",
+          "Deseja realmente postar?",
+          [{
+            text: "Cancelar",
+            style: "cancel"
+          }, {
+            text: "OK", onPress: () => addPost(uri)
+          }],
+          { cancelable: false }
+        );
+      }
+      toogleStatus(false);
+    }
+
+    return (
+      <Layout style={{ flex: 1 }}>
+        <ScrollView>
+          <CameraApp status={photoReady} onTakeCamera={(uri) => uploadPhoto(uri)} />
+
+          {photoReady == false && <Button onPress={() => toogleStatus(true)}>Postar</Button>}
+
+          {photoReady == false && posts.map((post, index) => (
+            <Card key={index} style={styles.card}>
+              <View style={styles.header}>
+                <Avatar
+                  size={'small'}
+                  source={{ uri: post.author.avatar }}
+                  style={styles.avatar} />
+                <Text style={styles.title}>{post.author.name}</Text>
+              </View>
+              <Image style={styles.picture} source={{ uri: post.image }} />
+              <Divider />
+              <View style={styles.footer}>
+                <Text style={styles.title}>{post.description}</Text>
+              </View>
+            </Card>))
+          }
+        </ScrollView>
+      </Layout >);
+  }
+}
+const styles = StyleSheet.create({
+  card: { padding: 1, margin: 4, backgroundColor: 'black' },
+  header: {
+    padding: 3,
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  scrollView: {
+    backgroundColor: 'black',
+    color: 'white',
+    marginHorizontal: 20,
+  },
+  avatar: { marginRight: 5 },
+  picture: { width: 'auto', minHeight: 200, maxHeight: 500 },
+  footer: {
+    margin: 4,
+    padding: 4,
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  title: {
+    fontSize: 15,
+  },
+});
---
+++ src/stores/home.store.tsx
@@ -0,0 +1,56 @@
+import { action, observable } from 'mobx';
+
+import axios from 'axios';
+
+type Post = {
+  id: number;
+  image: string;
+  description: string;
+  authorId: number;
+  author: {
+    id: number,
+    name: string,
+    avatar: string
+  }
+}
+
+export default class HomeStore {
+
+  @observable photoReady: boolean = false;
+
+  @observable posts: Post[] = [];
+
+  @action getPosts = async () => {
+    try {
+      const { data: posts } = await axios.get<[Post]>('http://localhost:3000/feed?_expand=author');
+      this.posts = posts;
+    } catch (error) {
+      console.log(error);
+      this.posts = [];
+    }
+  }
+
+  @action addPost = (uriPhoto) => {
+    const post: Post = {
+      author: {
+        avatar: 'https://i1.wp.com/www.jornadageek.com.br/wp-content/uploads/2016/10/Batman-not2.jpg?resize=696%2C398&ssl=1',
+        id: 2,
+        name: 'batman'
+      },
+      authorId: 2,
+      description: 'minha foto',
+      id: this.posts.length + 1,
+      image: uriPhoto
+    };
+
+    this.posts.unshift(post);
+  }
+
+  @action toogleStatus = (status: boolean) => {
+    this.photoReady = status;
+  }
+
+}
+
+const homeStore = new HomeStore();
+export { homeStore };
